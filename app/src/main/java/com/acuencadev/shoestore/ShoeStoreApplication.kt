ce2a612d63142b96d251196d67f9c7ff006a18ed
Amador Cuenca <sphi02ac@gmail.com>
2020-12-24 12:53:18 -0300

    initial: Initial Commit

---
+++ app/src/main/java/com/acuencadev/shoestore/ShoeStoreApplication.kt
@@ -0,0 +1,13 @@
+package com.acuencadev.shoestore
+
+import android.app.Application
+import timber.log.Timber
+
+class ShoeStoreApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
+}
