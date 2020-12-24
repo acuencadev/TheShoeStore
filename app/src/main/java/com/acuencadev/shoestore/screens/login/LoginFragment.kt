516f3a3fae292d7acbd001b12521f244bfcbdb45
Amador Cuenca <sphi02ac@gmail.com>
2020-12-24 14:54:19 -0300

    feat: Prototype login functionality
    - Create email and password labels and texts
    - Create the register and login buttons
    - User navigates to the welcome screen after touching the buttons

--- app/src/main/java/com/acuencadev/shoestore/screens/login/LoginFragment.kt
+++ app/src/main/java/com/acuencadev/shoestore/screens/login/LoginFragment.kt
@@ -5,15 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.acuencadev.shoestore.R
+import com.acuencadev.shoestore.databinding.FragmentLoginBinding
 
 /**
  * Fragment for the login screen of the app
  */
 class LoginFragment : Fragment() {
 
+    private lateinit var binding: FragmentLoginBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_login,
+            container,
+            false)
+
+        binding.loginButton.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+        }
+
+        binding.registerButton.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())     
+        }
+
+        return binding.root
+    }
+
+    fun navigateToWelcomeScreen(view: View) {
+        Toast.makeText(activity, "Navigate to Welcome screen", Toast.LENGTH_SHORT).show()
     }
 }
