a55be8310c2183b77abc68b8588987f088968b6d
Amador Cuenca <sphi02ac@gmail.com>
2020-12-24 14:58:28 -0300

    refactor: remove unused debug method

--- app/src/main/java/com/acuencadev/shoestore/screens/login/LoginFragment.kt
+++ app/src/main/java/com/acuencadev/shoestore/screens/login/LoginFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.acuencadev.shoestore.R
@@ -31,13 +30,9 @@ class LoginFragment : Fragment() {
         }
 
         binding.registerButton.setOnClickListener {
-            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())     
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
         }
 
         return binding.root
-    }
-
-    fun navigateToWelcomeScreen(view: View) {
-        Toast.makeText(activity, "Navigate to Welcome screen", Toast.LENGTH_SHORT).show()
     }
 }
